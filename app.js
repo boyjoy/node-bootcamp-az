99403cefb61ebbd707522a43affae930fb0d2ce5
Timothy Strimple <tstrimple@gmail.com>
2013-06-01 11:38:59 -0700

    socket.io example

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
 
-var app = express();
+app.listen(3000);
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    res.writeHead(200);
+    res.end(data);
+  });
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+io.set('log level', 0);
+io.sockets.on('connection', function (socket) {
+  var count = 0;
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+setInterval(function() {
+  count += 1;
+  socket.emit('count', count);
+}, 1000)
+
 });
+
+
+
+
+
+
