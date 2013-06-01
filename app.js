79ee2615c76357880c1c1b5dc04cf4b0841c7c08
Timothy Strimple <tstrimple@gmail.com>
2013-06-01 10:50:06 -0700

    added basic file caching.

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ var http = require('http'),
 	fs = require('fs'),
 	path = require('path');
 
+var cachedFiles = {};
+
 http.createServer(function(req, res) {
 	var file = path.join(__dirname, req.url);
+
+	var cachedFile = cachedFiles[file];
+	if(cachedFile) {
+		console.log('serving cached file: ', req.url);
+		res.end(cachedFile);
+		return;
+	}
 
 	fs.readFile(file, function(err, content) {
 		if(err) {
@@ -11,7 +20,23 @@ http.createServer(function(req, res) {
 			res.end("Could not find the file");
 			return;
 		}
+
+		fs.watch(file, function(e) {
+			if(e == 'change') {
+				fs.readFile(file, function(err, content) {
+				if(err) {
+					delete cachedFiles[file];
+					return;
+				}
+
+					console.log('refreshing cache', req.url);
+					cachedFiles[file] = content;
+				});
+			}
+		});
+
+		console.log('serving new file', req.url);
+		cachedFiles[file] = content;
 		res.end(content);
 	});
-
 }).listen(3000);
